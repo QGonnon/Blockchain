15ede4fb2b7427240d7e130fe179529490044a62
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-04-24 10:16:39 +0200

    removed unused variable

--- projet/ignition/modules/QuentinHeritERC20.js
+++ projet/ignition/modules/QuentinHeritERC20.js
@@ -3,13 +3,11 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const NAME = "QuentinHeritERC20";
 const SYMBOL = "QH";
 const DECIMALS = 0;
-const ONE_GWEI = 1_000_000_000n;
 
 module.exports = buildModule("QuentinHeritERC20Module", (m) => {
   const name = m.getParameter("name", NAME);
   const symbol = m.getParameter("symbol", SYMBOL);
   const decimals = m.getParameter("decimals", DECIMALS);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
   const quentinHeritERC20 = m.contract("QuentinHeritERC20", [name, symbol, decimals]);
 
